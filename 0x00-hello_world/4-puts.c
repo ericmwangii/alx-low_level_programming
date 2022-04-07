02566b08b8db32068bc977136df3cd7f5504fafa
ericmwangii <ericmwangii101@gmail.com>
2022-04-08 00:03:54 +0300

    betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,12 @@
 /**
 * main - Entry point
 *
-* Return 0
+* Return: 0 on success
 */
+
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
