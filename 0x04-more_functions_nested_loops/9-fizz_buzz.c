33d62468304f639d5263b87ec9dc33c270b2bc71
ericmwangii <ericmwangii101@gmail.com>
2022-04-14 12:32:20 +0300

    betty

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,17 +9,17 @@ int main(void)
 {
 int numbers;
 
-for(numbers = 1; numbers <= 100; numbers++)
+for (numbers = 1; numbers <= 100; numbers++)
 {
-if(numbers % 3 == 0 && numbers % 5 == 0)
+if (numbers % 3 == 0 && numbers % 5 == 0)
 {
 printf("FizzBuzz");
 }
-else if(numbers % 3 == 0)
+else if (numbers % 3 == 0)
 {
 printf("Fizz");
 }
-else if(numbers % 5 == 0)
+else if (numbers % 5 == 0)
 {
 printf("Buzz");
 }
@@ -27,11 +27,11 @@ else
 {
 printf("%d", numbers);
 }
-if(numbers != 100)
+if (numbers != 100)
 {
 printf(" ");
 }
 }
 printf("\n");
-return(0);  
+return (0);
 }
