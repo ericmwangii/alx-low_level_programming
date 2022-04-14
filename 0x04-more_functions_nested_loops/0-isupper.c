ee26016f5e2e8b0b9e4e5366170dd073ed6eb9fc
ericmwangii <ericmwangii101@gmail.com>
2022-04-14 11:07:00 +0300

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+   * _isupper - Entry point
+   * @c: The character to check
+   *Description: checks for uppercase character
+   *Return: 1 if c is uppercase and 0 otherwise
+*/
+
+int _isupper(int c)
+{
+return (c >= 65 && c <= 90);
+}
