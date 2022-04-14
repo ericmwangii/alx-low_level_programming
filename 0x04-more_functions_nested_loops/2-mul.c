c00b24ca98d38ed431938f5e40e2f8c31ff1f6f1
ericmwangii <ericmwangii101@gmail.com>
2022-04-14 11:23:46 +0300

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+   * mul - Entry point
+   * @a: integer
+   * @b: integer
+   *Description: multipy two integers
+   *Return: result
+*/
+
+int mul(int a, int b)
+{
+return (a * b);
+}
