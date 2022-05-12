f5a7cb189fc939288ba8a625c5ac26a7a347b1a5
ericmwangii <ericmwangii101@gmail.com>
2022-05-12 19:47:54 +0300

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_
+#define DOG_
+
+/**
+ * struct dog - dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
