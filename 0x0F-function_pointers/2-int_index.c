6599a68e443801c0ff3b69e6e81ba3489f964a31
ericmwangii <ericmwangii101@gmail.com>
2022-05-16 14:13:09 +0300

    int index final

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,14 +15,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
 
-if (array && cmp)
-{
-if (size <= 0)
+if (size < 1 || array == NULL || cmp == NULL)
 return (-1);
 
 for (i = 0; i < size; i++)
-if (cmp[array[i]])
+{
+if (cmp(array[i]))
 return (i);
 }
+
 return (-1);
 }
