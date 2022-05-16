04fe4f6fe02d17aeee1faca9c84b20a439af45b4
ericmwangii <ericmwangii101@gmail.com>
2022-05-16 13:53:26 +0300

    calc

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,7 +5,8 @@
  * @array: array of integeres
  * @size: size of array
  * @cmp: pointer to function
- * Return: the index of the first element for which the cmp function does not return 0
+ * Return: the index of the first element for which the cmp
+ * function does not return 0
  * If no element matches, return -1
  * If size <= 0, return -1
  */
@@ -21,7 +22,7 @@ return (-1);
 
 for (i = 0; i < size; i++)
 if (cmp[array[i]])
-return(i);   
+return (i);
 }
 return (-1);
 }
