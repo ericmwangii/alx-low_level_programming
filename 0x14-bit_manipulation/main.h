e4a6947a28cc989cdae8f5aeaceb28cb3b2fc390
ericmwangii <ericmwangii101@gmail.com>
2022-05-31 13:23:15 +0300

    print binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,9 @@
 #ifndef BITMANIPULATION
 #define BITMANIPULATION
 
+int _putchar(char c);
 unsigned int binary_to_uint(char *b);
+void print_binary(unsigned long int n);
 
 
 #endif /*BITMANIPULATION*/
